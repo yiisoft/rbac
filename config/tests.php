06ad202c23dd61a45c3a4764317d5398ea799dc5
Andrii Vasyliev <sol@hiqdev.com>
2018-12-27 11:12:40 +0000

    Added `tests` config

---
+++ config/tests.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    \yii\rbac\BaseManager::class => [
+        '__class' => \yii\rbac\DbManager::class,
+    ],
+    'db' => [
+        'dsn' => '/tmp/rbac-test',
+        'username' => 'rbac',
+        'password' => 'rbac',
+    ],
+];
