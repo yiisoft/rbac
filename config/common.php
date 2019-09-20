17e219da4db861ee11a6cc21aa00336190aea44b
Alexander Makarov <sam@rmcreative.ru>
2019-09-20 15:37:10 +0300

    Remove configs

--- config/common.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    \Yiisoft\Rbac\Factory\RuleFactoryInterface::class => [
-        '__class' => \Yiisoft\Rbac\Factory\RuleFactory::class,
-    ],
-    \Yiisoft\Access\AccessCheckerInterface::class => [
-        '__class' => \Yiisoft\Rbac\DenyAll::class,
-    ],
-];
