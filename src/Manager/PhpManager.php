4ee0b495d0e6090fb580f5ca2a0dbc1bb54d589a
romkatsu <r@tsurkanu.ru>
2020-02-09 12:06:36 +0000

    revert php manager

--- src/Manager/PhpManager.php
+++ src/Manager/PhpManager.php
@@ -763,7 +763,14 @@ class PhpManager extends BaseManager
         $items = [];
         foreach ($this->items as $name => $item) {
             /* @var $item Item */
-            $items[$name] = array_filter($item->getAttributes());
+            $items[$name] = array_filter(
+                [
+                    'type' => $item->getType(),
+                    'description' => $item->getDescription(),
+                    'ruleName' => $item->getRuleName(),
+                    // FIXME: 'data' => $item->data,
+                ]
+            );
             if (isset($this->children[$name])) {
                 foreach ($this->children[$name] as $child) {
                     /* @var $child Item */
