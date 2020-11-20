9461325f9c111d5d6a8d0e08da778ff4cc68d0f0
Alexander Makarov <samdark@users.noreply.github.com>
2020-11-20 21:25:53 +0000

    Apply fixes from StyleCI

--- src/StorageInterface.php
+++ src/StorageInterface.php
@@ -18,25 +18,30 @@ interface StorageInterface
 
     /**
      * Returns all items in the system.
+     *
      * @return Item[] All items in the system.
      */
     public function getItems(): array;
 
     /**
      * Returns the named item.
+     *
      * @param string $name The item name.
+     *
      * @return Item|null The item corresponding to the specified name. Null is returned if no such item.
      */
     public function getItemByName(string $name): ?Item;
 
     /**
      * Adds the item to RBAC system.
+     *
      * @param Item $item The item to add.
      */
     public function addItem(Item $item): void;
 
     /**
      * Updates the specified role, permission or rule in the system.
+     *
      * @param string $name The old name of the role, permission or rule.
      * @param Item $item Modified item.
      */
@@ -44,6 +49,7 @@ interface StorageInterface
 
     /**
      * Removes a role, permission or rule from the RBAC system.
+     *
      * @param Item $item Item to remove.
      */
     public function removeItem(Item $item): void;
@@ -55,13 +61,16 @@ interface StorageInterface
 
     /**
      * Returns all roles in the system.
+     *
      * @return Role[] All roles in the system.
      */
     public function getRoles(): array;
 
     /**
      * Returns the named role.
+     *
      * @param string $name The role name.
+     *
      * @return Role|null The role corresponding to the specified name. Null is returned if no such role.
      */
     public function getRoleByName(string $name): ?Role;
@@ -74,13 +83,16 @@ interface StorageInterface
 
     /**
      * Returns all permissions in the system.
+     *
      * @return Permission[] All permissions in the system.
      */
     public function getPermissions(): array;
 
     /**
      * Returns the named permission.
+     *
      * @param string $name The permission name.
+     *
      * @return Permission|null The permission corresponding to the specified name. Null is returned if no such permission.
      */
     public function getPermissionByName(string $name): ?Permission;
@@ -95,19 +107,23 @@ interface StorageInterface
      * Returns the child permissions and/or roles.
      *
      * @param string $name The parent name.
+     *
      * @return Item[] The child permissions and/or roles.
      */
     public function getChildrenByName(string $name): array;
 
     /**
      * Returns whether named parent has children.
+     *
      * @param string $name The parent name.
+     *
      * @return bool Whether named parent has children.
      */
     public function hasChildren(string $name): bool;
 
     /**
      * Adds an item as a child of another item.
+     *
      * @param Item $parent Parent to add child to.
      * @param Item $child Child to add.
      */
@@ -116,6 +132,7 @@ interface StorageInterface
     /**
      * Removes a child from its parent.
      * Note, the child item is not deleted. Only the parent-child relationship is removed.
+     *
      * @param Item $parent Parent to remove child from.
      * @param Item $child Child to remove.
      */
@@ -124,20 +141,25 @@ interface StorageInterface
     /**
      * Removed all children form their parent.
      * Note, the children items are not deleted. Only the parent-child relationships are removed.
+     *
      * @param Item $parent Parent to remove children from.
      */
     public function removeChildren(Item $parent): void;
 
     /**
      * Returns all role assignment information.
+     *
      * @psalm-return array<string,array<string, Assignment>>
+     *
      * @return array
      */
     public function getAssignments(): array;
 
     /**
      * Returns all role assignment information for the specified user.
+     *
      * @param string $userId The user ID.
+     *
      * @return Assignment[] The assignments. An empty array will be
      * returned if there is no role assigned to the user.
      */
@@ -145,14 +167,17 @@ interface StorageInterface
 
     /**
      * Returns role assignment for the specified item name that belongs to user with the specified ID.
+     *
      * @param string $userId The user ID.
      * @param string $name Role name.
+     *
      * @return Assignment|null Assignment or null if there is no role assigned to the user.
      */
     public function getUserAssignmentByName(string $userId, string $name): ?Assignment;
 
     /**
      * Adds assignment of the role to the user with ID specified.
+     *
      * @param string $userId The user ID.
      * @param Item $item Role to assign.
      */
@@ -160,13 +185,16 @@ interface StorageInterface
 
     /**
      * Returns whether there is assignment for a named role or permission.
+     *
      * @param string $name Name of the role or the permission.
+     *
      * @return bool Whether there is assignment.
      */
     public function assignmentExist(string $name): bool;
 
     /**
      * Removes assignment of a role to the user with ID specified.
+     *
      * @param string $userId The user ID.
      * @param Item $item Role to remove assignment to.
      */
@@ -174,6 +202,7 @@ interface StorageInterface
 
     /**
      * Removes all role assignments for a user with ID specified.
+     *
      * @param string $userId The user ID.
      */
     public function removeAllAssignments(string $userId): void;
@@ -185,25 +214,30 @@ interface StorageInterface
 
     /**
      * Returns all rules available in the system.
+     *
      * @return Rule[] The rules indexed by the rule names.
      */
     public function getRules(): array;
 
     /**
      * Returns the rule of the specified name.
+     *
      * @param string $name The rule name.
+     *
      * @return Rule|null The rule object, or null if the specified name does not correspond to a rule.
      */
     public function getRuleByName(string $name): ?Rule;
 
     /**
      * Removes the rule of the specified name from RBAC system.
+     *
      * @param string $name The rule name.
      */
     public function removeRule(string $name): void;
 
     /**
      * Adds the rule to RBAC system.
+     *
      * @param Rule $rule The rule to add.
      */
     public function addRule(Rule $rule): void;
