85d8a8c9448ca3f6a8c3d27d0995b448278e7e96
Valerii Gorbachev <darkdef@gmail.com>
2021-10-05 20:28:17 +0300

    remove line in CHANGELOG.md

---
+++ tests/FakeAssignmentsStorage.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac\Tests;
+
+use Yiisoft\Rbac\Assignment;
+use Yiisoft\Rbac\AssignmentsStorageInterface;
+use Yiisoft\Rbac\Item;
+
+final class FakeAssignmentsStorage implements AssignmentsStorageInterface
+{
+    private array $assignments = [];
+
+    public function getAssignments(): array
+    {
+        return $this->assignments;
+    }
+
+    public function getUserAssignments(string $userId): array
+    {
+        return $this->assignments[$userId] ?? [];
+    }
+
+    public function getUserAssignmentByName(string $userId, string $name): ?Assignment
+    {
+        return $this->getUserAssignments($userId)[$name] ?? null;
+    }
+
+    public function addAssignment(string $userId, Item $item): void
+    {
+        $this->assignments[$userId][$item->getName()] = new Assignment(
+            $userId,
+            $item->getName(),
+            time()
+        );
+    }
+
+    public function assignmentExist(string $name): bool
+    {
+        foreach ($this->getAssignments() as $assignmentInfo) {
+            foreach ($assignmentInfo as $itemName => $assignment) {
+                if ($itemName === $name) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function removeAssignment(string $userId, Item $item): void
+    {
+        unset($this->assignments[$userId][$item->getName()]);
+    }
+
+    public function removeAllAssignments(string $userId): void
+    {
+        $this->assignments[$userId] = [];
+    }
+
+    public function clearAssignments(): void
+    {
+        $this->assignments = [];
+    }
+
+    public function updateAssignmentsForItemName(string $name, Item $item): void
+    {
+        foreach ($this->assignments as &$assignments) {
+            if (isset($assignments[$name])) {
+                $assignments[$item->getName()] = $assignments[$name]->withItemName($item->getName());
+                unset($assignments[$name]);
+            }
+        }
+    }
+
+    public function removeAssignmentsFromItem(Item $item): void
+    {
+        $this->clearAssignmentsFromItem($item);
+    }
+
+    private function clearAssignmentsFromItem(Item $item): void
+    {
+        foreach ($this->assignments as &$assignments) {
+            unset($assignments[$item->getName()]);
+        }
+    }
+}
