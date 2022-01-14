bef37ba36dd894bf4f0d01dbf75b32c392b51810
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-01-14 18:24:40 +0300

    Fix

--- tests/Support/AllowRule.php
+++ tests/Support/AllowRule.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Tests\Support;
 
+use Yiisoft\Rbac\Item;
 use Yiisoft\Rbac\Rule;
 
 final class AllowRule extends Rule
 {
+    public function execute(string $userId, Item $item, array $parameters = []): bool
+    {
+        return true;
+    }
 }
