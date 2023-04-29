ca32e4016a354ef575727a2324793ae10b24cbd4
Alexey Rogachev <arogachev90@gmail.com>
2023-04-29 13:32:35 +0600

    Update SchemaManagerTrait

--- src/SchemaManagerTrait.php
+++ src/SchemaManagerTrait.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac;
 
+use InvalidArgumentException;
+
 trait SchemaManagerTrait
 {
+    /**
+     * @var string A name of the table for storing RBAC items (roles and permissions).
+     * @psalm-var non-empty-string
+     */
+    private string $itemsTable;
+    /**
+     * @var string A name of the table for storing RBAC assignments.
+     * @psalm-var non-empty-string
+     */
+    private string $assignmentsTable;
+    /**
+     * @var string A name of the table for storing relations between RBAC items.
+     * @psalm-var non-empty-string
+     */
+    private string $itemsChildrenTable;
+
     public function createAll(bool $force = true): void
     {
         if ($force === true) {
@@ -22,5 +40,26 @@ trait SchemaManagerTrait
         $this->dropTable($this->itemsChildrenTable);
         $this->dropTable($this->assignmentsTable);
         $this->dropTable($this->itemsTable);
+    }
+
+    private function initTables(string $itemsTable, string $assignmentsTable, string $itemsChildrenTable): void
+    {
+        if ($itemsTable === '') {
+            throw new InvalidArgumentException('Items table name can\'t be empty.');
+        }
+
+        $this->itemsTable = $itemsTable;
+
+        if ($assignmentsTable === '') {
+            throw new InvalidArgumentException('Assignments table name can\'t be empty.');
+        }
+
+        $this->assignmentsTable = $assignmentsTable;
+
+        if ($itemsChildrenTable === '') {
+            throw new InvalidArgumentException('Items children table name can\'t be empty.');
+        }
+
+        $this->itemsChildrenTable = $itemsChildrenTable ?? $this->itemsTable . '_child';
     }
 }
