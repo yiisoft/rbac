76ff0791eb1445e49cfaf81cf54507371a40a46f
Alexey Rogachev <arogachev90@gmail.com>
2023-04-29 12:35:14 +0600

    Add SchemaManagerInterface

---
+++ src/SchemaManagerInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac;
+
+interface SchemaManagerInterface
+{
+    public function createItemsTable(): void;
+
+    public function createItemsChildrenTable(): void;
+
+    public function createAssignmentsTable(): void;
+
+    public function tableExists(string $tableName): bool;
+
+    public function dropTable(string $tableName): void;
+
+    public function createAll(): void;
+
+    public function dropAll(): void;
+}
