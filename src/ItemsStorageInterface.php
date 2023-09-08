aae858b60f47fa50361dd7621db88510c8422e10
Alexey Rogachev <arogachev90@gmail.com>
2023-09-08 13:16:18 +0600

    Psalm - simplify type

--- src/ItemsStorageInterface.php
+++ src/ItemsStorageInterface.php
@@ -7,7 +7,7 @@ namespace Yiisoft\Rbac;
 /**
  * A storage for RBAC roles and permissions used in {@see Manager}.
  *
- * @psalm-type ItemsIndexedByName = array<string, Permission|Role>
+ * @psalm-type ItemsIndexedByName = array<string, Item>
  */
 interface ItemsStorageInterface
 {
