d2bdce73fb0be241988c335e716469e2c755a8e3
Alexey Rogachev <arogachev90@gmail.com>
2023-11-22 13:01:15 +0600

    Add dedicated test for rule context to increase coverage

---
+++ tests/RuleContextTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Rbac\RuleContext;
+use Yiisoft\Rbac\Tests\Support\EasyRule;
+use Yiisoft\Rbac\Tests\Support\SimpleRuleFactory;
+
+final class RuleContextTest extends TestCase
+{
+    public function testGetParameters(): void
+    {
+        $context = new RuleContext(new SimpleRuleFactory(['easy' => new EasyRule()]), ['a' => 1, 'b' => 2]);
+        $this->assertSame(['a' => 1, 'b' => 2], $context->getParameters());
+    }
+
+    public function testGetParameterValue(): void
+    {
+        $context = new RuleContext(new SimpleRuleFactory(['easy' => new EasyRule()]), ['a' => 1, 'b' => 2]);
+        $this->assertSame(1, $context->getParameterValue('a'));
+        $this->assertSame(null, $context->getParameterValue('c'));
+    }
+
+    public function testHasParameter(): void
+    {
+        $context = new RuleContext(new SimpleRuleFactory(['easy' => new EasyRule()]), ['a' => 1, 'b' => 2]);
+        $this->assertTrue($context->hasParameter('a'));
+        $this->assertFalse($context->hasParameter('c'));
+    }
+
+    public function testCreateRule(): void
+    {
+        $context = new RuleContext(new SimpleRuleFactory(['easy' => new EasyRule()]), ['a' => 1, 'b' => 2]);
+        $this->assertEquals(new EasyRule(), $context->createRule('easy'));
+    }
+}
