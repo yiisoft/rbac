65049a1ca27cd874d6702173dc3f148f52db11e7
Alexey Rogachev <arogachev90@gmail.com>
2023-12-18 18:39:10 +0600

    Handle empty assignments `Manager::getPermissionsByUserId()`

--- src/Manager.php
+++ src/Manager.php
@@ -222,6 +222,10 @@ final class Manager implements ManagerInterface
     {
         $userId = (string) $userId;
         $assignments = $this->assignmentsStorage->getByUserId($userId);
+        if (empty($assignments)) {
+            return [];
+        }
+
         $assignmentNames = array_keys($assignments);
 
         return array_merge(
