cb2eb5bf2377da586fcaf0d1a3c60cfc77085168
Alexey Rogachev <arogachev90@gmail.com>
2024-01-25 13:59:19 +0600

    Add `Assignment::getAttributes()` method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -52,6 +52,7 @@
 - Bug #222: Adjust hierarchy when removing item (@arogachev)
 - Bug #223: Handle empty assignments in `Manager::getPermissionsByUserId()` (@arogachev)
 - Enh #227: Use snake case for item attribute names (ease migration from Yii 2) (@arogachev)
+- New #?: Add `Assignment::getAttributes()` method (@arogachev)
 
 ## 1.0.2 April 20, 2023
 
--- tests/AssignmentTest.php
+++ tests/AssignmentTest.php
@@ -11,9 +11,19 @@ final class AssignmentTest extends TestCase
 {
     public function testImmutability(): void
     {
-        $original = new Assignment('42', 'test1', 1_642_029_084);
+        $original = new Assignment(userId: '42', itemName: 'test1', createdAt: 1_642_029_084);
         $new = $original->withItemName('test2');
 
         $this->assertNotSame($original, $new);
+    }
+
+    public function testGetAttributes(): void
+    {
+        $assignment = new Assignment(userId: '42', itemName: 'test1', createdAt: 1_642_029_084);
+        $this->assertSame([
+            'item_name' => 'test1',
+            'user_id' => '42',
+            'created_at' => 1642029084,
+        ], $assignment->getAttributes());
     }
 }
